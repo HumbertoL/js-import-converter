2cf5e25aefe49b971cf30827eb7263dcc8a58243
Humberto <humberto@call-em-all.com>
2022-12-09 21:13:04 -0600

    feat: added import pattern matching

--- index.js
+++ index.js
@@ -6,16 +6,47 @@ const path = require('path');
 // open directory
 const clientRootDir = '../cea-desktop/client/src';
 
+function processFile(dirPath, file) {
+  const filePath = path.join(dirPath, file.name);
+  // console.log('filePath', filePath);
+
+  // get file extension
+  const ext = path.extname(filePath);
+  if (ext === '.js' || ext === '.jsx') {
+    // read file into string
+    const content = fs.readFileSync(filePath);
+
+    // import { TextField } from '@mui/material';
+
+    // find regex match
+    // const regex = /^import\s+\{(.*)\}\s+from\s+\'\@mui\/material\';/gm;
+
+    // Global, multiline, single line (dot matches new line)
+    const regex = /^import\s+\{([^{]*)\}\s+from\s+\'\@mui\/material\';$/gms;
+
+    const match = regex.exec(content);
+    if (match) {
+      // console.log('match', match);
+      const importMatch = match[0];
+      const importList = match[1];
+
+      console.log('importMatch ', importMatch);
+      // console.log('importList', importList);
+    }
+  }
+}
+
 function traverseDir(dir, level = 0) {
   let dirent = dir.readSync();
   while (dirent) {
     // console.log('dirent', dirent);
-    const indent = ' '.repeat(level * 2);
+    // const indent = ' '.repeat(level * 2);
     const filePath = path.join(dir.path, dirent.name);
 
     // Check if the entry is a file or a directory
     if (dirent.isFile()) {
-      console.log('F:', indent, filePath);
+      // console.log('F:', indent, filePath);
+      processFile(dir.path, dirent);
     } else if (dirent.isDirectory()) {
       // console.log("D:", indent, filePath);
       const subDir = fs.opendirSync(filePath);
